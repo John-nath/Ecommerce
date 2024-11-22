56d60d9334fa20e0dc3a8097254b242d3b08f974
john-nath <jadikwu22@gmail.com>
2024-11-22 12:12:18 +0100

    Policy correction

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollections from '../components/LatestCollections'
 import BestSeller from '../components/BestSeller'
+import OurPolicy   from '../components/OurPolicy.jsx'
+
 import NewsletterBox from '../components/NewsletterBox'
-import OurPolicy from '../components/ourPolicy'
+
 
 const Home = () => {
   return (
