b1c11f4b604cc929b801261927838b5f90514f97
john-nath <jadikwu22@gmail.com>
2024-11-22 12:21:52 +0100

    Cart Total resolution

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollections from '../components/LatestCollections'
 import BestSeller from '../components/BestSeller'
-import OurPolicy   from '../components/OurPolicy.jsx'
+import OurPolicy   from '../components/OurPolicy'
 
 import NewsletterBox from '../components/NewsletterBox'
 
